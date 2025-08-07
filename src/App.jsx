f3ff69161b38eeede05b0f7bff4c0e2e825266e3
Sarah Kristian Perry <sarah.perry@keyin.com>
2025-08-07 08:22:24 -0230

    Refactor App component to use updated API endpoints for fetching fish catches

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [tournaments, setTournaments] = useState([]);
+  const [fishCatch, setFishCatch] = useState([]);
 
   const handleClick = async () => {
     console.log('Button clicked');
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/tournament`);
-      if (!res.ok) throw new Error('Failed to fetch tournaments');
+      const res = await fetch(
+        `${import.meta.env.VITE_CATCH_ENDPOINT}/available`
+      );
+      if (!res.ok) throw new Error('Failed to fetch catches');
 
       const data = await res.json();
-      setTournaments(data);
-      console.log('tournaments returned:', data);
+      setFishCatch(data);
+      console.log('fish catches returned:', data);
     } catch (err) {
       console.error('Error:', err);
     }
@@ -23,10 +25,10 @@ function App() {
     <>
       <div>
         <h1>My Demo Frontend</h1>
-        <button onClick={handleClick}>Load Tournaments</button>
+        <button onClick={handleClick}>See Available Catches</button>
 
         <ul>
-          {tournaments.map((t, index) => (
+          {fishCatch.map((t, index) => (
             <li key={index}>{t.name}</li>
           ))}
         </ul>
