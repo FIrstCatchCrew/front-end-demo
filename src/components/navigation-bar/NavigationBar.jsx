dbd018e5820ff6477c1afd9e05aff9340bd8958f
Gerogie18 <jennlyver@gmail.com>
2025-08-09 19:00:43 -0230

    chore: add page not found skeleton

---
+++ src/components/navigation-bar/NavigationBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+  return <nav>
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/available-catches">Available Catches</Link></li>
+      <li><Link to="/page-not-found">Page Not Found</Link></li>
+    </ul>
+  </nav>;
+};
+
+
+export default NavigationBar;
