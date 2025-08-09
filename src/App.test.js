a7e2662586a83c8d478ea892a11a2bce41264d31
Gerogie18 <jennlyver@gmail.com>
2025-08-09 15:12:29 -0230

    test: app

---
+++ src/App.test.js
@@ -0,0 +1,16 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+
+import '@testing-library/jest-dom';
+
+describe('Application Smoke Test', () => {
+  it('should render the main App component without errors', () => {
+    render(<App />);
+
+   // const titleElement = screen.getByText()
+
+    expect(titleElement).toBeInTheDocument();
+  });
+});
